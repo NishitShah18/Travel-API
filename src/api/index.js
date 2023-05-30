5c17b51db5d775062ff7bcb93c408529c48d06c3
Nishit Shah <ngshah3@myseneca.ca>
2023-05-30 15:47:55 -0400

    Add files via upload

---
+++ src/api/index.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const options = {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY, // Your Rapid API Key
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
+    };
+
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      options
+    );
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const options = {
+      params: { lon: lng, lat: lat },
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY, // Your Rapid API Key
+        "X-RapidAPI-Host": "community-open-weather-map.p.rapidapi.com",
+      },
+    };
+
+    const { data } = await axios.get(
+      "https://community-open-weather-map.p.rapidapi.com/climate/month",
+      options
+    );
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
